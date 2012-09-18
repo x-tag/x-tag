ed6b3e03a323829da0a19e895d319ab9500a74df
Arron Schaar <aschaar@gmail.com>
2012-09-17 20:47:12 -0700

    jasmine is setup and added a few tests

--- test/jasmine/spec/tests.js
+++ test/jasmine/spec/tests.js
@@ -1,64 +1,83 @@
 
-describe("X-Tag ", function() {
-	var iframe = document.createElement('iframe');
-	iframe.onload = function(){
-		console.log('foo');
-	}
-	var win;
-	var doc;
-	var head;
-	var body;
-	
-	beforeEach(function() {
-		if (win) {
-			win.location = 'about:blank';
-		}
-		else {
-			iframe.src = 'about:blank';
-			document.body.appendChild(iframe);
-			win = iframe.contentWindow;
-			doc = iframe.contentDocument;
-			head = doc.getElementsByTagName('head')[0];
-			body = doc.body;
-			body.innerHTML = "FUUUUUUUUUUUUUUUUUCK!";
-		}
-		console.log(doc, head, body);
-	});
+describe("x-tag ", function() {
 
-	it("should fire DOMComponentsLoaded", function() {
+	it('should load x-tag and fire DOMComponentsLoaded', function(){
 		var componentsLoaded = false;
-		console.log(head);
-		doc.addEventListener('DOMComponentsLoaded', function(event){
+		document.addEventListener('DOMComponentsLoaded', function(){
 			componentsLoaded = true;
 		});
-		
+		runs(function(){
+			var script = document.createElement('script');
+			script.type = 'text/javascript';
+			script.src = '../../x-tag.js';
+			document.getElementsByTagName('head')[0].appendChild(script);
+		});
+
 		waitsFor(function() {
 			return componentsLoaded;
-		}, "The document should be loaded", 2000);
+		}, "The document should be loaded", 1000);
 		
 		runs(function() {
-			console.log('test');
-			expect('DOMComponentsLoaded').toEqual('DOMComponentsLoaded');
+			expect(window.xtag).toBeDefined();
 		});
-		
-		var script = doc.createElement('script');
-			script.type = 'text/javascript';
-			script.src = '../../../x-tag.js';
-		head.appendChild(script);
 	});
-	
-/* 	describe("when included ", function() {
-		
-		beforeEach(function() {
-			var script = doc.createElement('script');
-				script.type = 'text/javascript';
-				script.src = '../../../x-tag.js';
-			head.appendChild(script);
+
+	it('should register a new tag', function(){
+		xtag.register('x-foo', {});
+		expect(xtag.tags['x-foo']).toBeDefined();
+	});
+
+	it('should fire onCreate when a new tag is created', function(){
+		var onCreateFired = false;
+		xtag.register('x-foo', {
+			onCreate: function(){
+				onCreateFired = true;
+			}
 		});
-		
-		it("should fire DOMComponentsLoaded", function() {
-			expect(true).toEqual(true);
+
+		var foo = document.createElement('x-foo');
+
+		waitsFor(function(){
+			return onCreateFired;
+		}, "new tag onCreate should fire", 1000);
+
+		runs(function(){
+			expect(onCreateFired).toEqual(true);
 		});
+	});
+
+	describe('using testbox', function(){
+		var testBox;
 		
-	}); */
+		beforeEach(function(){
+			testBox = document.getElementById('testbox');
+		});
+
+		afterEach(function(){
+			testBox.innerHTML = "";
+		});	
+
+		it('testbox should exist', function(){
+			expect(testBox).toBeDefined();
+		});
+
+		it('should fire onInsert when injected into the DOM', function(){
+			var onInsertFired = false;
+			xtag.register('x-foo', {
+				onInsert: function(){
+					onInsertFired = true;
+				}
+			});
+
+			var foo = document.createElement('x-foo');
+			testbox.appendChild(foo);
+			waitsFor(function(){
+				return onInsertFired;
+			}, "new tag onInsert should fire", 1000);
+
+			runs(function(){
+				expect(onInsertFired).toEqual(true);
+			});
+		});
+	});
 });
